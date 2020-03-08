dbc34c23384a1a9fd2b295d010bb2ef1b0aaa32d
szpakusik <szpakusik@gmail.com>
2020-03-08 09:47:37 +0100

    simulate click event on button component

--- src/components/button/button.spec.js
+++ src/components/button/button.spec.js
@@ -21,14 +21,13 @@ describe('SharedButton Component', ()=>{
     describe('Renders', () => {
         
         let wrapper;
+        let mockFunc;
         beforeEach(()=>{
-
+            mockFunc = jest.fn()
             const props = {
 
                 buttonText: 'Example button Text',
-                emitEvent: ()=>{
-    
-                }
+                emitEvent: mockFunc,
             }
             wrapper = shallow(<SharedButton {...props} />);
 
@@ -37,6 +36,13 @@ describe('SharedButton Component', ()=>{
         it('Should Render a button', ()=>{
             const button = findByTestAttr( wrapper,'buttonComponent' );
             expect( button.length ).toBe(1);
+        })
+
+        it('Should emit a callback function on click event', ()=>{
+            const button = findByTestAttr( wrapper,'buttonComponent' );
+            button.simulate('click');
+            const callback = mockFunc.mock.calls.length;
+            expect(callback).toBe(1);
         })
 
     })
